7a4575c0f2a09585c292ec968ec260bdb89465c5
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2024-02-09 22:13:42 +0800

    Added styling on Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,46 +1,54 @@
 import React from "react";
 import { FaRegCircleUser, FaLock, FaEye } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
-const inputControl = "px-8 py-2 border-b-2 border-slate-700";
+const inputControl = "px-8 py-2 border-b-2 border-slate-700 w-full";
 
 const Login = () => {
 	return (
-		<div className="flex flex-col items-center justify-center self-center">
-			<h1>LOGIN</h1>
-			<form className="flex flex-col">
-				<label htmlFor="email" className="text-lg">
-					Email
-				</label>
-				<div className="relative">
-					<FaRegCircleUser className="absolute top-2 left-1 text-2xl " />
-					<input
-						type="text"
-						name="email"
-						id="email"
-						placeholder="Enter your email"
-						className={inputControl}
-					/>
+		<div className="flex flex-col items-center self-center h-screen mt-20 w-[500px] gap-3">
+			<h1 className="pb-5 text-4xl font-bold w-full text-center fons-serif underline">
+				LOGIN
+			</h1>
+			<form className="flex flex-col gap-5 w-full">
+				<div>
+					<label htmlFor="email" className="text-xl">
+						Email
+					</label>
+					<div className="relative">
+						<FaRegCircleUser className="absolute top-2 left-1 text-xl " />
+						<input
+							type="text"
+							name="email"
+							id="email"
+							placeholder="Enter your email"
+							className={inputControl}
+						/>
+					</div>
 				</div>
-				<label htmlFor="password" className="text-lg">
-					Password
-				</label>
-				<div className="relative">
-					<FaLock className="absolute top-2 left-1 text-xl " />
-					<input
-						type="password"
-						name="password"
-						id="password"
-						placeholder="Enter your password"
-						className={inputControl}
-					/>
-					<FaEye className="absolute top-2 right-2 text-xl cursor-pointer"/>
+				<div>
+					<label htmlFor="password" className="text-xl">
+						Password
+					</label>
+					<div className="relative">
+						<FaLock className="absolute top-3 left-1 text-lg " />
+						<input
+							type="password"
+							name="password"
+							id="password"
+							placeholder="Enter your password"
+							className={inputControl}
+						/>
+						<FaEye className="absolute top-3 right-2 text-xl cursor-pointer hover:opacity-70 transition-opacity" />
+					</div>
 				</div>
-
-				<button type="submit">LOGIN</button>
+				<button type="submit" className="bg-slate-900 text-white p-2 rounded-sm text-lg hover:bg-slate-800 focus:bg-slate-800 transition-colors">
+					LOGIN
+				</button>
 			</form>
-			<div>
-				<p>No account yet?</p>
-				<button>SIGN UP</button>
+			<div className="w-full text-center">
+				<p>Don't have an  account yet?</p>
+				<Link to={"/register"} className="font-bold hover:underline hover:text-blue-500 focus:underline focus:text-blue-500 ">Sign up</Link>
 			</div>
 		</div>
 	);
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router-dom"
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+	return (
+		<div className="flex flex-col items-center self-center h-screen mt-20 w-[500px] gap-3">
+			<h1 className="pb-5 text-4xl font-bold w-full text-center fons-serif underline">
+				REGISTER
+			</h1>
+			<form className="flex flex-col gap-5 w-full">
+				<div>
+					<h1>Name</h1>
+					<div>
+						<input type="text" name="firstName" id="firstName" />
+						<label htmlFor="firstName">First</label>
+					</div>
+					<div>
+						<input type="text" name="lastName" id="lastName" />
+						<label htmlFor="lastName">Last</label>
+					</div>
+				</div>
+				<div>
+					<label htmlFor="address">Address</label>
+					<input type="text" name="address" id="address" />
+				</div>
+				<div>
+					<label htmlFor="email">Email</label>
+					<input type="text" name="email" id="email" />
+				</div>
+				<div>
+					<div>
+						<label htmlFor="password">Password</label>
+						<input type="password" name="password" id="password" />
+					</div>
+					<div>
+						<label htmlFor="password2">Confirm Password</label>
+						<input type="password" name="password2" id="password2" />
+					</div>
+				</div>
+				<button
+					type="submit"
+					className="bg-slate-900 text-white p-2 rounded-sm text-lg hover:bg-slate-800 focus:bg-slate-800 transition-colors"
+				>
+					REGISTER
+				</button>
+			</form>
+			<div className="w-full text-center">
+				<p>Already have an account?</p>
+				<Link
+					to={"/login"}
+					className="font-bold hover:underline hover:text-blue-500 focus:underline focus:text-blue-500 "
+				>
+					Login
+				</Link>
+			</div>
+		</div>
+	);
+};
 
-export default Register
+export default Register;
