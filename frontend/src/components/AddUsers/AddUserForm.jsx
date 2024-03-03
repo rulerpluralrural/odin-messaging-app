a5475e10ffcc4ec9cf97e343568e7d137c88a410
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2024-03-03 22:05:15 +0800

    Edit AddUserForm and UsersCard components

--- frontend/src/components/AddUsers/AddUserForm.jsx
+++ frontend/src/components/AddUsers/AddUserForm.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FaRegPlusSquare } from "react-icons/fa";
 import { FaMagnifyingGlass, FaXmark } from "react-icons/fa6";
-import { PulseLoader } from "react-spinners";
 import UsersCard from "./UsersCard";
 
 const AddUserForm = ({ setPopupAddUser }) => {
@@ -30,7 +29,7 @@ const AddUserForm = ({ setPopupAddUser }) => {
 
 	return (
 		<div className="fixed inset-0 h-screen bg-slate-700 bg-opacity-80 flex items-center justify-center z">
-			<div className="flex flex-col gap-3 bg-white p-10 w-[500px] aspect-square relative shadow-sm shadow-slate-700 rounded-md overflow-scroll">
+			<div className="flex flex-col gap-3 bg-white p-10 relative shadow-sm shadow-slate-700 rounded-md overflow-scroll w-[500px]">
 				<FaXmark
 					onClick={() => {
 						setPopupAddUser(false);
@@ -38,44 +37,62 @@ const AddUserForm = ({ setPopupAddUser }) => {
 					className="absolute top-3 right-4 text-red-600 cursor-pointer text-xl font-bold hover:text-red-700"
 				/>
 				<div className="text-center ">
-					<h1 className="font-bold text-lg">Add a User</h1>
+					<h1 className="font-bold text-xl font-serif">Add a User</h1>
 				</div>
-				<div className="relative w-full mt-2">
-					<FaMagnifyingGlass className="absolute top-3 left-3 text-slate-600 text-lg" />
-					<input
-						type="text"
-						className="border-2 border-slate-400 rounded-md px-10 py-2 w-full"
-						placeholder="Search..."
-						onChange={(e) => {
-							searchUser(e.target.value);
-						}}
-					></input>
-				</div>
-				<div className="flex flex-col gap-2">
-					{usersFound.length > 0 ? (
-						usersFound.map((user, index) => {
-							return (
-								<UsersCard
-									key={index}
-									user={user}
-									selectedUser={selectedUser}
-									setSelectedUser={setSelectedUser}
-								/>
-							);
-						})
-					) : (
-						<div className="flex-1 h-full self-center">
-							<p className="text-center text-slate-600 py-10 text-xl font-Roboto">No users found!</p>
-						</div>
-					)}
-				</div>
+				<Searchbar searchUser={searchUser} />
+				<SearchResult
+					usersFound={usersFound}
+					selectedUser={selectedUser}
+					setSelectedUser={setSelectedUser}
+				/>
 				<button
 					type="button"
-					className="w-[200px] self-center flex items-end justify-center gap-2 bg-blue-500 text-white cursor-pointer p-3 rounded-md focus:bg-blue-600 hover:bg-blue-600 transition-colors"
+					className="w-full self-center flex items-end justify-center gap-2 bg-blue-500 text-white cursor-pointer p-3 rounded-md focus:bg-blue-600 hover:bg-blue-600 transition-colors"
 				>
 					Add <FaRegPlusSquare className="text-2xl" />
 				</button>
 			</div>
+		</div>
+	);
+};
+
+const Searchbar = ({ searchUser }) => {
+	return (
+		<div className="relative w-full">
+			<FaMagnifyingGlass className="absolute top-3 left-3 text-slate-600 text-lg" />
+			<input
+				type="text"
+				className="border-2 border-slate-400 rounded-md px-10 py-2 w-full"
+				placeholder="Search..."
+				onChange={(e) => {
+					searchUser(e.target.value);
+				}}
+			></input>
+		</div>
+	);
+};
+
+const SearchResult = ({ usersFound, selectedUser, setSelectedUser }) => {
+	return (
+		<div className="flex flex-col items-center justify-center gap-2 h-[300px] pt-7 px-4 aspect-square overflow-scroll w-full">
+			{usersFound.length > 0 ? (
+				usersFound.map((user, index) => {
+					return (
+						<UsersCard
+							key={index}
+							user={user}
+							selectedUser={selectedUser}
+							setSelectedUser={setSelectedUser}
+						/>
+					);
+				})
+			) : (
+				<div className="flex flex-col gap-2 aspect-square overflow-scroll w-full">
+					<p className="text-center text-slate-600 py-7 rounded-md text-xl font-Roboto border-2 border-slate-400 w-full">
+						No users found!
+					</p>
+				</div>
+			)}
 		</div>
 	);
 };
--- frontend/src/components/AddUsers/UsersCard.jsx
+++ frontend/src/components/AddUsers/UsersCard.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 
 const UsersCard = ({ user, selectedUser, setSelectedUser }) => {
-    console.log(selectedUser)
 	return (
 		<div
-			className={`flex gap-2 items-center bg-slate-100 text-slate-900 p-2 rounded-md cursor-pointer shadow-sm shadow-slate-300 ${
-				selectedUser === user.handle && "bg-blue-300"
-			}`}
+			className={`${
+				selectedUser === user.handle ? "bg-blue-500 text-white" : "bg-slate-100"
+			} flex gap-2 items-center text-slate-900 p-2 rounded-md cursor-pointer shadow-sm shadow-slate-300 w-full hover:bg-blue-500 group transition-colors`}
 			onClick={() => {
 				setSelectedUser(user.handle);
 			}}
@@ -16,11 +15,21 @@ const UsersCard = ({ user, selectedUser, setSelectedUser }) => {
 				alt={`${user.firstName} Img`}
 				className="rounded-full aspect-square bg-white text-xs text-slate-500 flex items-center justify-center w-[60px]"
 			/>
-			<div className="flex flex-col gap-1 font-Roboto">
+			<div
+				className={`${
+					selectedUser === user.handle ? "text-white font-bold" : "text-black font-normal"
+				} flex flex-col gap-1 font-Roboto group-hover:text-white group-hover:font-bold`}
+			>
 				<p>
 					{user.firstName} {user.lastName}
 				</p>
-				<p className="text-slate-600">{user.handle}</p>
+				<p
+					className={`${
+						selectedUser === user.handle ? "text-white font-normal" : "text-slate-600"
+					} group-hover:text-white group-hover:font-normal`}
+				>
+					{user.handle}
+				</p>
 				<p className="font-bold">{user.work}</p>
 			</div>
 		</div>
