71605b8ec2fa98a4f03db7c51af570d847c2440d
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2024-03-05 22:01:00 +0800

    Fixed user messages in Message.jsx

--- frontend/src/components/Messages/ChatBox.jsx
+++ frontend/src/components/Messages/ChatBox.jsx
@@ -139,13 +139,14 @@ const ChatBoxMessages = ({ selectedRoom, user }) => {
 					<p className="text-slate-700">There are no messages in this room!</p>
 				</div>
 			) : (
-				<div>
+				<div className="px-4 py-2">
 					{selectedRoom.messages.map((item, index) => {
 						return (
 							<Message
 								key={index}
 								message={item.message}
-								sender={item.sender.firstName}
+								senderID={item.sender._id}
+								senderName={item.sender.firstName}
 								profileImg={item.sender.profileImg}
 								dateSent={item.time_formatted}
 								user={user}
--- frontend/src/components/Messages/Message.jsx
+++ frontend/src/components/Messages/Message.jsx
@@ -1,48 +1,72 @@
 import React from "react";
 
-const Message = ({ message, sender, profileImg, dateSent, user }) => {
+const Message = ({
+	message,
+	senderID,
+	senderName,
+	profileImg,
+	dateSent,
+	user,
+}) => {
 	return (
-		<div className="flex gap-2 items-center">
-			<div className="rounded-full border-[1px] border-slate-800 aspect-square w-[50px] flex items-center self-start">
-				<img
-					src={profileImg}
-					alt={`${sender} .jpeg`}
-					className="object-fit text-xs text-center"
+		<div className="flex items-center">
+			{user._id === senderID ? (
+				<UserMessage
+					dateSent={dateSent}
+					message={message}
+					profileImg={profileImg}
+					senderName={senderName}
 				/>
-			</div>
-			{user.name === sender ? (
-				<UserMessage dateSent={dateSent} message={message} />
 			) : (
-				<OtherMessage dateSent={dateSent} message={message} sender={sender} />
+				<OtherMessage
+					dateSent={dateSent}
+					message={message}
+					senderName={senderName}
+					profileImg={profileImg}
+				/>
 			)}
 		</div>
 	);
 };
 
-const UserMessage = ({ dateSent, message }) => {
+const UserMessage = ({ dateSent, message, profileImg, senderName }) => {
 	return (
-		<div className="flex flex-col justify-between gap-1">
-			<div className="flex items-center gap-2">
-				<p className="font-bold font-Roboto">You</p>
-				<small className="text-xs  text-slate-500">{dateSent}</small>
+		<div className="flex gap-2 py-5">
+			<div className="rounded-full border-[1px] border-slate-800 aspect-square w-[50px] flex items-center self-start">
+				<img
+					src={profileImg}
+					alt={`${senderName} .jpeg`}
+					className="object-fit text-xs text-center"
+				/>
 			</div>
-			<div  className="bg-blue-500 text-white font-sans tracking-wide px-5 py-3 rounded-md rounded-tl-none">
-				<p>{message}</p>
+			<div className="flex flex-col justify-between gap-1">
+					<p className="font-bold font-Roboto">You</p>
+				<div className="bg-blue-500 text-white font-sans tracking-wide px-5 py-3 rounded-md rounded-tl-none">
+					<p>{message}</p>
+				</div>
 			</div>
+			<small className="text-sm  text-slate-500 self-end">{dateSent}</small>
 		</div>
 	);
 };
 
-const OtherMessage = ({ dateSent, message, sender }) => {
+const OtherMessage = ({ dateSent, message, senderName, profileImg }) => {
 	return (
-		<div className="flex flex-col justify-between gap-1">
-			<div className="flex items-center gap-2">
-				<p className="font-bold font-Roboto">{sender}</p>
-				<small className="text-xs  text-slate-500">{dateSent}</small>
+		<div className=" flex flex-row-reverse gap-2 self-start w-full py-5">
+			<div className="rounded-full border-[1px] border-slate-800 aspect-square w-[50px] flex items-center self-start">
+				<img
+					src={profileImg}
+					alt={`${senderName} .jpeg`}
+					className="object-fit text-xs text-center"
+				/>
 			</div>
-			<div  className="bg-blue-500 text-white font-sans tracking-wide px-5 py-3 rounded-md rounded-tl-none">
-				<p>{message}</p>
+			<div className="flex flex-col justify-between gap-1">
+					<p className="font-bold font-Roboto self-end">{senderName}</p>
+				<div className="bg-slate-100 text-slate-900 font-sans tracking-wide px-5 py-3 rounded-md rounded-tr-none">
+					<p>{message}</p>
+				</div>
 			</div>
+			<small className="text-sm text-slate-500 self-end">{dateSent}</small>
 		</div>
 	);
 };
