105a703e5d52a29b1bcdc9426b43666f3da97cec
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2024-03-07 22:46:07 +0800

    Minor edit on Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AccessDenied from "./AccessDenied";
 
 const Profile = ({ user }) => {
-
+	console.log(user);
 	if (!user) {
 		return <AccessDenied />;
 	}
@@ -10,11 +10,32 @@ const Profile = ({ user }) => {
 	return (
 		<div className="flex flex-col">
 			<div className="flex items-center justify-center py-10">
-        <div>
-            <img src={user.profileImg} alt={`${user.name}.jpg`} className="rounded-full h-[300px] aspect-square object-cover  border-4 border-white shadow-md shadow-slate-400"/>
-        </div>
-      
-      </div>
+				<div className="flex flex-col text-center gap-2">
+					<img
+						src={user.profileImg}
+						alt={`${user.name}.jpg`}
+						className="rounded-full h-[300px] w-[300px] object-cover  border-4 border-white shadow-md shadow-slate-400 self-center"
+					/>
+					<div className="w-[500px] ">
+						<div className="flex gap-2 items-center justify-center">
+							<h1 className="text-xl font-Roboto">
+								{user.firstName} {user.lastName}
+							</h1>
+							<a
+								className="text-blue-700 text-sm cursor-pointer hover:text-blue-800 hover:underline"
+								title="Twitter"
+								href="https://twitter.com/"
+								target="_blank"
+							>
+								{user.handle}
+							</a>
+						</div>
+
+						<p className="text-2xl font-bold">{user.work}</p>
+						<p>{user.about}</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
